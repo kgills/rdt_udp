1b1a742f3ab2c41349f0c241998b9db13d9dc268
Kevin Gillespie <kgills@gmail.com>
2017-07-15 21:24:19 -0500

    Basic file transfer working.

--- client.py
+++ client.py
@@ -1,13 +1,37 @@
+#! /usr/bin/env python
 import socket
+import signal
+import sys
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!"
+def signal_handler(signal, frame):
+    sys.exit(0)
+
+################################################################################
+# Main
+
+signal.signal(signal.SIGINT, signal_handler)
+
+# Parse the input arguments
+usage = "python client.py <path_to_file> server_ip server_port"
+if len(sys.argv) < 4:
+    print usage
+    sys.exit(1)
+
+UDP_IP = sys.argv[2]
+UDP_PORT = int(sys.argv[3])
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
 
+# Get a byte array of the file
+with open(sys.argv[1], "rb") as sendFile:
+    f = sendFile.read()
+    byteArray = bytearray(f)
+
+print "Sending: ", byteArray
+
+# Open the socket
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+# Send a test message
+sock.sendto(byteArray, (UDP_IP, UDP_PORT))
--- server.py
+++ server.py
@@ -1,7 +1,23 @@
+#! /usr/bin/env python
 import socket
+import signal
+import sys
+
+def signal_handler(signal, frame):
+    print "Closing server"
+    sys.exit(0)
+
+
+################################################################################
+# Main
+
+signal.signal(signal.SIGINT, signal_handler)
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
+
+print "UDP Server IP:", UDP_IP
+print "UDP Server port:", UDP_PORT
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
@@ -9,4 +25,8 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    print "Received: ", data
+
+    # Write byte array to file
+    with open("output.txt", 'wb') as output:
+        output.write(data)
