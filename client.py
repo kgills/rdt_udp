e5545f95dd6619e7b61536041d84334c2ea50ad0
Kevin Gillespie <kgills@gmail.com>
2017-07-16 13:51:58 -0500

    Packets of size MSS are working with CRC error checking.

--- client.py
+++ client.py
@@ -3,8 +3,33 @@ import socket
 import signal
 import sys
 
+################################################################################
+# Definitions
+
 def signal_handler(signal, frame):
     sys.exit(0)
+
+def crc16(crc, data):
+    msb = crc >> 8
+    lsb = crc & 255
+    for c in data:
+        x = c ^ msb
+        x ^= (x >> 4)
+        msb = (lsb ^ (x >> 3) ^ (x << 4)) & 255
+        lsb = (x ^ (x << 5)) & 255
+    return (msb << 8) + lsb
+
+CRC_SEED = 0x1021
+HEADER_LEN = 5
+
+HEADER_LEN = 5
+MSS = 100
+PACKET_LEN = MSS+HEADER_LEN
+
+FLAGS_POS = 0
+FLAGS_FIN = 0x01
+
+CRC_POS = 3
 
 ################################################################################
 # Main
@@ -14,24 +39,58 @@ signal.signal(signal.SIGINT, signal_handler)
 # Parse the input arguments
 usage = "python client.py <path_to_file> server_ip server_port"
 if len(sys.argv) < 4:
-    print usage
+    print(usage)
     sys.exit(1)
 
 UDP_IP = sys.argv[2]
 UDP_PORT = int(sys.argv[3])
 
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
+print ("UDP target IP:", UDP_IP)
+print ("UDP target port:", UDP_PORT)
 
 # Get a byte array of the file
 with open(sys.argv[1], "rb") as sendFile:
     f = sendFile.read()
-    byteArray = bytearray(f)
+    data = bytearray(f)
 
-print "Sending: ", byteArray
+data_len = len(data)
+i = 0
+pos = 0
 
-# Open the socket
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-# Send a test message
-sock.sendto(byteArray, (UDP_IP, UDP_PORT))
+while(i < data_len):
+
+    # Isolate the packet
+    i += MSS
+    data_temp = bytearray(data[pos:i])
+    pos = i
+
+    # Add the header
+    if(i >= data_len):
+        flags = FLAGS_FIN
+    else:
+        flags = 0
+    seq = 0
+    window = 0
+    crc = 0
+
+    # Add in reverse order
+    data_temp = crc.to_bytes(2, 'big') + data_temp
+    data_temp = window.to_bytes(1, 'big') + data_temp
+    data_temp = seq.to_bytes(1, 'big') + data_temp
+    data_temp = flags.to_bytes(1, 'big') + data_temp
+
+    crc = crc16(CRC_SEED, data_temp)
+
+    # Replace the CRC Value
+    data_temp = bytearray(data_temp)
+    data_temp[CRC_POS] = (crc & 0xFF00) >> 8
+    data_temp[CRC_POS+1] = (crc & 0x00FF) >> 0
+
+    # Open the socket
+    sock = socket.socket(socket.AF_INET, # Internet
+                         socket.SOCK_DGRAM) # UDP
+    # Send the packet
+    sock.sendto(data_temp, (UDP_IP, UDP_PORT))
+
+print("Done sending file")
+sock.close()
--- server.py
+++ server.py
@@ -3,30 +3,84 @@ import socket
 import signal
 import sys
 
+################################################################################
+# Definitions
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+
+CRC_SEED = 0x1021
+HEADER_LEN = 5
+MSS = 100
+PACKET_LEN = MSS+HEADER_LEN
+
+FLAGS_POS = 0
+FLAGS_FIN = 0x01
+
+CRC_POS = 3
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
 def signal_handler(signal, frame):
-    print "Closing server"
+    print("\nClosing server")
+    sock.close()
     sys.exit(0)
 
+def crc16(crc, data):
+    msb = crc >> 8
+    lsb = crc & 255
+    for c in data:
+        x = c ^ msb
+        x ^= (x >> 4)
+        msb = (lsb ^ (x >> 3) ^ (x << 4)) & 255
+        lsb = (x ^ (x << 5)) & 255
+    return (msb << 8) + lsb
 
 ################################################################################
 # Main
 
 signal.signal(signal.SIGINT, signal_handler)
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+print("UDP Server IP:", UDP_IP)
+print("UDP Server port:", UDP_PORT)
 
-print "UDP Server IP:", UDP_IP
-print "UDP Server port:", UDP_PORT
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
-
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "Received: ", data
 
-    # Write byte array to file
-    with open("output.txt", 'wb') as output:
-        output.write(data)
+    # Clear the data buffer
+    data = bytearray()
+
+    # Clear the crc_error flag
+    crc_error = 0
+
+    while True:
+        data_temp, addr = sock.recvfrom(PACKET_LEN)
+        data_temp = bytearray(data_temp)
+
+        # Check the CRC
+        crc = (int(data_temp[CRC_POS]) << 8) + (int(data_temp[CRC_POS+1]) << 0)
+
+        # Clear the CRC field before the calculation
+        data_temp[CRC_POS] = 0
+        data_temp[CRC_POS+1] = 0
+        crc_calc = crc16(CRC_SEED, data_temp)
+        if(crc_calc != crc):
+            crc_error = 1
+            print("CRC Error! packet_crc:", hex(crc), "calc_crc:",hex(crc_calc))
+            break;
+
+        # Add the data
+        data += data_temp[5:]
+
+        # Break if this is the last packet
+        if ((data_temp[FLAGS_POS] & FLAGS_FIN) == FLAGS_FIN):
+            break;
+
+    if(crc_error == 0):
+        print("Writing file")
+        # Write byte array to file
+        with open("output.png", 'wb') as output:
+            output.write(data)
+
