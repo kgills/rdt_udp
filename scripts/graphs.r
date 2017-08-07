3e16c4c811a6529809eb4495c889a255c6eed1fb
Kevin Gillespie <kgills@gmail.com>
2017-08-06 21:57:35 -0500

    Adding graphs and finishing report.

--- scripts/graphs.r
+++ scripts/graphs.r
@@ -2,6 +2,7 @@ library(gridExtra)
 
 data_1k = read.csv("../data/1k_results.txt")
 data_100k = read.csv("../data/100k_1_results.txt")
+data_100k_2 = read.csv("../data/100k_2_results.txt")
 
 # Aggregate the bits per second and the window size
 data_1k_ag = aggregate(bps~window, data_1k, mean)
@@ -14,12 +15,11 @@ plot(bps~window, data_100k_ag, main="BPS vs Window Size", xlab="Window Size",
 dev.off()
 
 # Aggregate the bits per second and the latency
-data_100k_ag = aggregate(bps~latency, data_100k, mean)
-data_1k_ag = aggregate(bps~latency, data_1k, mean)
+data_100k_ag = aggregate(bps~latency, data_100k_2, mean)
 
 # Create the plots
-png("1k_lateny.png", width=600, height=500)
-plot(bps~latency, data_1k_ag, main="BPS vs Latency", xlab="Latency ms", 
+png("100k_lateny.png", width=600, height=500)
+plot(bps~latency, data_100k_ag, main="BPS vs Latency", xlab="Latency ms", 
 	ylab="Bits Per Second", col="green", pch=16)
 dev.off()
 
@@ -33,10 +33,10 @@ plot(bps~mss, data_100k_ag, main="BPS vs MSS", xlab="MSS bytes",
 dev.off()
 
 # Aggregate the bits per second and the drop percentage
-data_100k_ag = aggregate(bps~mss, data_100k, mean)
+data_100k_ag = aggregate(bps~loss, data_100k_2, mean)
 
 # Create the plots
-png("100k_mss.png", width=600, height=500)
-plot(bps~mss, data_100k_ag, main="BPS vs MSS", xlab="MSS bytes", 
-	ylab="Bits Per Second", col="blue", pch=16)
+png("100k_loss.png", width=600, height=500)
+plot(bps~loss, data_100k_ag, main="BPS vs Drop %", xlab="Drop %", 
+	ylab="Bits Per Second", col="purple", pch=16)
 dev.off()
